e0b86d4924132298a9e52dd5883e0ea4f02f7f23
Jesús Horcajo <jesushorcajo@outlook.com>
2023-12-20 17:50:27 +0100

    Minor changes on tests to match new text

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -2,19 +2,19 @@ import { test, expect } from '@playwright/test';
 
 test('info is loaded and Pokemon appear on the main page', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('td').filter({ hasText: 'bulbasaur' })).toHaveText('bulbasaur');
+  await expect(page.locator('td').filter({ hasText: 'Bulbasaur' })).toHaveText('Bulbasaur');
 })
 
 test('app should navigate back to the home page from a detail view', async ({ page }) => {
   await page.goto('/');
-  await page.getByText('bulbasaur').click();
+  await page.getByText('Bulbasaur').click();
   await page.getByLabel('Go back to main page').click();
   await expect(page).toHaveURL('/')
 });
 
 test('Detailed information of a Pokémon is displayed when selected', async ({ page }) => {
   await page.goto('/');
-  await page.getByText('bulbasaur').click();
+  await page.getByText('Bulbasaur').click();
   await expect(page).toHaveURL('/pokemon/bulbasaur')
   await expect(page.getByText('BULBASAUR')).toBeVisible();
 });
@@ -23,31 +23,31 @@ test('Pokémon can be filtered by type', async ({ page }) => {
   await page.goto('/');
   await page.getByRole('button', { name: 'Open' }).click();
   await page.getByRole('listbox').getByText('water').click(); 
-  await expect(page.getByText('bulbasaur')).toBeHidden();
-  await expect(page.getByText('squirtle')).toBeVisible();
+  await expect(page.getByText('Bulbasaur')).toBeHidden();
+  await expect(page.getByText('Squirtle')).toBeVisible();
 });
 
 test('Pokémon can be filtered by name', async ({ page }) => {
   await page.goto('/');
   await page.getByLabel('Search Pokemon by name').click();
   await page.getByLabel('Search Pokemon by name').fill('squirtle');
-  await expect(page.getByText('bulbasaur')).toBeHidden();
-  await expect(page.getByText('squirtle')).toBeVisible();
+  await expect(page.getByText('Bulbasaur')).toBeHidden();
+  await expect(page.getByText('Squirtle')).toBeVisible();
 });
 
 test('Pokémon can be filtered by id', async ({ page }) => {
   await page.goto('/');
   await page.getByLabel('Search Pokemon by name').click();
   await page.getByLabel('Search Pokemon by name').fill('30');
-  await expect(page.getByText('bulbasaur')).toBeHidden();
-  await expect(page.getByText('nidorina')).toBeVisible();
+  await expect(page.getByText('Bulbasaur')).toBeHidden();
+  await expect(page.getByText('Nidorina')).toBeVisible();
 });
 
 test('Table will show an error message if no Pokemon fit the search', async ({ page }) => {
   await page.goto('/');
   await page.getByLabel('Search Pokemon by name').click();
   await page.getByLabel('Search Pokemon by name').fill('loremipsum');
-  await expect(page.getByText('bulbasaur')).toBeHidden();
+  await expect(page.getByText('Bulbasaur')).toBeHidden();
   await expect(page.getByRole('cell', { name: 'No data available' })).toBeVisible();
   await page.getByLabel('Search Pokemon by name').fill('3000');
   await expect(page.getByRole('cell', { name: 'No data available' })).toBeVisible();
@@ -63,15 +63,15 @@ test('Pokémon table paginator can be changed', async ({ page }) => {
 test('Pokémon table name column is sorted', async ({ page }) => {
   await page.goto('/');
   await page.getByRole('cell', { name: 'Name', exact: true }).locator('i').click();
-  await expect(page.getByText('abra')).toBeVisible();
+  await expect(page.getByText('Abra')).toBeVisible();
   await page.getByRole('cell', { name: 'Name', exact: true }).locator('i').click();
-  await expect(page.getByText('zubat')).toBeVisible();
+  await expect(page.getByText('Zubat')).toBeVisible();
 });
 
 test('Page is accessible through keyboard navigation', async ({ page }) => {
   await page.goto('/');
-  await page.getByText('bulbasaur').press('Tab');
-  await page.getByText('ivysaur').press('Enter');
+  await page.getByText('Bulbasaur').press('Tab');
+  await page.getByText('Ivysaur').press('Enter');
   await expect(page).toHaveURL('/pokemon/ivysaur')
   await page.locator('body').press('Tab');
   await page.getByLabel('Go back to main page').press('Enter');
